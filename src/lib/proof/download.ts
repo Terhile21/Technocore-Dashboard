4a129220e839c22575ddaa9ad1a31eea6240ca39
Terhile <162378687+Terhile21@users.noreply.github.com>
2026-08-29 13:43:24 -0700

    Update download.ts

--- src/lib/proof/download.ts
+++ src/lib/proof/download.ts
@@ -3,6 +3,9 @@ export function downloadTextFile(filename: string, content: string): void {
   const anchor = document.createElement("a");
   anchor.href = url;
   anchor.download = filename;
+  anchor.style.display = "none";
+  document.body.appendChild(anchor);
   anchor.click();
-  URL.revokeObjectURL(url);
+  document.body.removeChild(anchor);
+  window.setTimeout(() => URL.revokeObjectURL(url), 0);
 }
